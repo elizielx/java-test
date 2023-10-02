e90c0e089babafd493e8309b04909a639bb2a4df
elizielx <yehezkieldio35@gmail.com>
2023-10-02 13:37:47 +0800

    initial: initial

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        long startTime = System.currentTimeMillis();
+        System.out.println("Hello, World!");
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+    }
+}
